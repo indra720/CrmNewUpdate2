d1c1187b17bea59b1c27f0bb3072c5e94368e366
Indrajeet <indrajeetdhaked720@gmail.com>
2026-01-23 17:20:03 +0530

    url change

--- src/components/pms/Sprints.tsx
+++ src/components/pms/Sprints.tsx
@@ -335,7 +335,7 @@ export function Sprints() {
   };
 
   return (
-    <div className="container mx-auto flex flex-col min-h-screen  p-4 sm:p-6 lg:p-8 space-y-8 bg-card">
+    <div className="container mx-auto flex flex-col min-h-screen p-4 sm:p-6 lg:p-8 space-y-8">
       {/* Header Section */}
       <div className="flex flex-col sm:flex-row sm:items-center justify-between gap-4">
         <div>
